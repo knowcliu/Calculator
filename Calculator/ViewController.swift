411c0d96fa4a65376da4a6ad6059631124bf54d6
Caroline Liu <CarolineAnthony@gmail.com>
2015-08-21 01:14:47 -0400

    Basic Calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func clear() {
+        userIsInTheMiddleOfTypingANumber = false
+        brain.clear()
+        displayValue = 0
     }
-
-
+    
+    @IBAction func decimalPoint() {
+        if userIsInTheMiddleOfTypingANumber {
+            if display.text?.rangeOfString(".") == nil {
+                display.text = display.text! + "."
+            }
+        } else {
+            display.text = "0."
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func pi() {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        displayValue = M_PI
+        enter()
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+             displayValue = 0
+        }
+    }
+    
+    var displayValue: Double! {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)?.doubleValue
+        }
+        
+        set{
+            if newValue == nil {
+                display.text = " "
+            } else {
+                display.text = "\(newValue)"
+            }
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
 }
 
